bf0a14a5a90c0add9d8395f738edcb1ec8a9280f
giftofhope10-tech <giftofhope10@gmail.com>
2026-08-29 02:01:50 +0400

    fix: single self-contained app config, splash via expo-splash-screen plugin

--- app.config.js
+++ app.config.js
@@ -1,14 +1,56 @@
-const appJson = require('./app.json');
-
 module.exports = {
-  ...appJson,
   expo: {
-    ...appJson.expo,
+    name: "HTML Viewer & Editor",
+    slug: "html-viewer-editor",
+    version: "1.3.0",
+    orientation: "default",
+    icon: "./assets/images/icon.png",
+    scheme: "htmlviewer",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.iftechstudio.html_live_editor",
+    },
+    android: {
+      package: "com.iftechstudio.html_live_editor",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/icon.png",
+        backgroundColor: "#0f172a",
+      },
+      permissions: [],
+      edgeToEdgeEnabled: true,
+      enableProguardInReleaseBuilds: true,
+      enableShrinkResourcesInReleaseBuilds: true,
+    },
+    web: {
+      bundler: "metro",
+      output: "single",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      "expo-font",
+      "expo-file-system",
+      "expo-document-picker",
+      "expo-sharing",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/icon.png",
+          resizeMode: "contain",
+          backgroundColor: "#0f172a",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
     extra: {
-      ...(appJson.expo.extra || {}),
       eas: {
-        ...(appJson.expo.extra?.eas || {}),
-        projectId: process.env.EAS_PROJECT_ID || appJson.expo.extra?.eas?.projectId || undefined,
+        projectId:
+          process.env.EAS_PROJECT_ID || "e18e27a5-760e-4ff2-b4de-439f47000438",
       },
     },
   },
